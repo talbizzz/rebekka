d1b4c683b7e327134fae5c4b32f0e184a9cf5164
Aziz Talbi <a.talbi890@gmail.com>
2020-12-17 09:37:45 +0100

    fixed social network and welcome page issuess

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -9,11 +9,11 @@ const Welcome = ({About, Gallery, Appointments, Contact}) => {
 		<div className="welcome">
 			<div className='welcomebg'>
 				<div 
-					className="fl w-25 tl mt5 mb7"
+					className="fl w-100 w-25-ns tc tl-ns mt3 mb3 mt5-ns mb7-ns"
 					data-aos="zoom-in" 
 					data-aos-duration="2000"
 				>
-					<a class="link gray hover-silver dib h2 w2 ml6" href="https://facebook.com" title="Facebook">
+					<a class="link gray hover-silver dib h2 w2 ml6-ns" href="https://facebook.com" title="Facebook">
 						<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M15.117 0H.883C.395 0 0 .395 0 .883v14.234c0 .488.395.883.883.883h7.663V9.804H6.46V7.39h2.086V5.607c0-2.066 1.262-3.19 3.106-3.19.883 0 1.642.064 1.863.094v2.16h-1.28c-1 0-1.195.476-1.195 1.176v1.54h2.39l-.31 2.416h-2.08V16h4.077c.488 0 .883-.395.883-.883V.883C16 .395 15.605 0 15.117 0" fill-rule="nonzero"/></svg>
 					</a>
 					<a class="link hover-silver gray dib h2 w2 ml3" href="https://instagram.com/mrmrs_" title="Instagram">
@@ -26,7 +26,7 @@ const Welcome = ({About, Gallery, Appointments, Contact}) => {
 						<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M13.632 13.635h-2.37V9.922c0-.886-.018-2.025-1.234-2.025-1.235 0-1.424.964-1.424 1.96v3.778h-2.37V6H8.51V7.04h.03c.318-.6 1.092-1.233 2.247-1.233 2.4 0 2.845 1.58 2.845 3.637v4.188zM3.558 4.955c-.762 0-1.376-.617-1.376-1.377 0-.758.614-1.375 1.376-1.375.76 0 1.376.617 1.376 1.375 0 .76-.617 1.377-1.376 1.377zm1.188 8.68H2.37V6h2.376v7.635zM14.816 0H1.18C.528 0 0 .516 0 1.153v13.694C0 15.484.528 16 1.18 16h13.635c.652 0 1.185-.516 1.185-1.153V1.153C16 .516 15.467 0 14.815 0z" fill-rule="nonzero"/></svg>
 					</a>
 				</div>
-				<div className ="fl w-75 tr-ns mt5 mv5-ns f3 f1-ns"style={{fontFamily:"Mak", color:"white"}}>
+				<div className ="fl w-100 w-75-ns tc tr-ns f3 f1-ns"style={{fontFamily:"Mak", color:"white"}}>
 					<div 
 						className="mr7-ns"
 						data-aos="fade-down" 
@@ -42,37 +42,38 @@ const Welcome = ({About, Gallery, Appointments, Contact}) => {
 						lyrischer Mezzosopran				
 					</div>
 				</div>
-				<div className ="pl3 pt1 mv6-ns ml6-ns f3-ns .aosAnchor "style={{color:"white", fontFamily:"Mak"}}>
+				<div className ="tc mv6-ns ml6-ns f3-ns .aosAnchor "style={{color:"white", fontFamily:"Mak"}}>
 					<div 
+						className="fl w-20 tc"
 						data-aos="fade-up-right" 
 						data-aos-duration="2100" 
 						data-aos-anchor=".aosAnchor" 
 					>
-						<a href="/" className="grow no-underline dib white fl pa2 pa2-ns w-100-ns" onClick={About}>About</a>
+						<a href="/" className="grow no-underline dib white fl pl2 pv2 w-100-ns" onClick={About}>About</a>
 					</div>
 					<div 
-						className=""
+						className="fl w-20 tc"
 						data-aos="fade-up-right" 
 						data-aos-duration="2300" 
 						data-aos-anchor=".aosAnchor" 
 					>
-						<a href="/" className="grow no-underline dib white fl pa2 pa2-ns w-100-ns" onClick={Gallery}>Gallery</a>
+						<a href="/" className="grow no-underline dib white fl pv2 w-100-ns" onClick={Gallery}>Gallery</a>
 					</div>
 					<div 
-						className=""
+						className="fl w-40 tc"
 						data-aos="fade-up-right" 
 						data-aos-duration="2600" 
 						data-aos-anchor=".aosAnchor" 
 					>
-						<a href="/" className="grow no-underline dib white fl pa2 pa2-ns w-100-ns" onClick={Appointments}>Appointments</a>
+						<a href="/" className="grow no-underline dib white fl pl2 pv2 w-100-ns" onClick={Appointments}>Appointments</a>
 					</div>
 					<div 
-						className=""
+						className="fl w-20 tc"
 						data-aos="fade-up-right" 
 						data-aos-duration="2900"
 						data-aos-anchor=".aosAnchor" 
 					>
-						<a href="/" className="grow no-underline dib white fl pa2 pa2-ns w-100-ns" onClick={Contact}>Contact</a>
+						<a href="/" className="grow no-underline dib white fl pv2 w-100-ns" onClick={Contact}>Contact</a>
 					</div>
 				</div>
 			</div>
