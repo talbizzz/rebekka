894ef0c22892650adccf91386a2aceb1ec5280e9
Aziz Talbi <a.talbi890@gmail.com>
2020-12-18 14:03:41 +0100

    Welcome page done

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './Welcome.css';
+import ReactPlayer from 'react-player';
 
 AOS.init();
 const Welcome = ({About, Gallery, Appointments, Contact}) => {
@@ -9,7 +10,7 @@ const Welcome = ({About, Gallery, Appointments, Contact}) => {
 		<div className="welcome">
 			<div className='welcomebg'>
 				<div 
-					className="fl w-100 w-25-ns tc tl-ns mt3 mb3 mt5-ns mb7-ns"
+					className="fl w-100 w-25-ns tc tl-ns mt3 mb3 mt5-ns mb6-ns"
 					data-aos="zoom-in" 
 					data-aos-duration="2000"
 				>
@@ -26,7 +27,7 @@ const Welcome = ({About, Gallery, Appointments, Contact}) => {
 						<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M13.632 13.635h-2.37V9.922c0-.886-.018-2.025-1.234-2.025-1.235 0-1.424.964-1.424 1.96v3.778h-2.37V6H8.51V7.04h.03c.318-.6 1.092-1.233 2.247-1.233 2.4 0 2.845 1.58 2.845 3.637v4.188zM3.558 4.955c-.762 0-1.376-.617-1.376-1.377 0-.758.614-1.375 1.376-1.375.76 0 1.376.617 1.376 1.375 0 .76-.617 1.377-1.376 1.377zm1.188 8.68H2.37V6h2.376v7.635zM14.816 0H1.18C.528 0 0 .516 0 1.153v13.694C0 15.484.528 16 1.18 16h13.635c.652 0 1.185-.516 1.185-1.153V1.153C16 .516 15.467 0 14.815 0z" fill-rule="nonzero"/></svg>
 					</a>
 				</div>
-				<div className ="fl w-100 w-75-ns tc tr-ns f3 f1-ns"style={{fontFamily:"Mak", color:"white"}}>
+				<div className ="fl w-100 w-75-ns tc tr-ns f3 f1-ns mt4-ns"style={{fontFamily:"Mak", color:"white"}}>
 					<div 
 						className="mr7-ns"
 						data-aos="fade-down" 
@@ -42,17 +43,17 @@ const Welcome = ({About, Gallery, Appointments, Contact}) => {
 						lyrischer Mezzosopran				
 					</div>
 				</div>
-				<div className ="tc mv6-ns ml6-ns f3-ns .aosAnchor "style={{color:"white", fontFamily:"Mak"}}>
+				<div className ="tl-ns tc ml6-ns f3-ns .aosAnchor"style={{color:"white", fontFamily:"Mak"}}>
 					<div 
-						className="fl w-20 tc"
+						className="fl w-20 w-100-ns tc tl-ns"
 						data-aos="fade-up-right" 
 						data-aos-duration="2100" 
 						data-aos-anchor=".aosAnchor" 
 					>
-						<a href="/" className="grow no-underline dib white fl pl2 pv2 w-100-ns" onClick={About}>About</a>
+						<a href="/" className="grow no-underline dib white fl pl0-ns pl2 pv2 w-100-ns" onClick={About}>About</a>
 					</div>
 					<div 
-						className="fl w-20 tc"
+						className="fl w-20 w-100-ns tc tl-ns"
 						data-aos="fade-up-right" 
 						data-aos-duration="2300" 
 						data-aos-anchor=".aosAnchor" 
@@ -60,20 +61,25 @@ const Welcome = ({About, Gallery, Appointments, Contact}) => {
 						<a href="/" className="grow no-underline dib white fl pv2 w-100-ns" onClick={Gallery}>Gallery</a>
 					</div>
 					<div 
-						className="fl w-40 tc"
+						className="fl w-40 w-100-ns tc tl-ns"
 						data-aos="fade-up-right" 
 						data-aos-duration="2600" 
 						data-aos-anchor=".aosAnchor" 
 					>
-						<a href="/" className="grow no-underline dib white fl pl2 pv2 w-100-ns" onClick={Appointments}>Appointments</a>
+						<a href="/" className="grow no-underline dib white fl pl0-ns pl2 pv2 w-100-ns" onClick={Appointments}>Appointments</a>
 					</div>
 					<div 
-						className="fl w-20 tc"
+						className="fl w-20 w-100-ns tc tl-ns"
 						data-aos="fade-up-right" 
 						data-aos-duration="2900"
 						data-aos-anchor=".aosAnchor" 
 					>
-						<a href="/" className="grow no-underline dib white fl pv2 w-100-ns" onClick={Contact}>Contact</a>
+						<a href="/" className="grow no-underline dib white fl pt2 w-100-ns mb7 mb6-ns" onClick={Contact}>Contact</a>
+					</div>
+					<div className={'ml4 ml7-ns pl6-ns'}>
+						<div className={"ml7-ns pl7-ns"}>
+							<ReactPlayer url='https://www.youtube.com/watch?v=X26O_Re_uK4' width={'320px'} height={'180px'} playing={true} />
+						</div>
 					</div>
 				</div>
 			</div>
